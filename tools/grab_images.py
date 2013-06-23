455a4810cbf9ff9fbefabd24862fdb94af7d9368
Zach Thomae <zthomae@gmail.com>
2013-06-23 17:18:06 -0500

    Finish mirroring images

--- tools/grab_images.py
+++ tools/grab_images.py
@@ -2,8 +2,8 @@
 
 import bs4
 import os
+import requests
 import shutil
-import urllib
 
 def grab_images(file_in, file_out):
     if not os.path.isdir('img'):
@@ -13,17 +13,23 @@ def grab_images(file_in, file_out):
         tree = bs4.BeautifulSoup(f_string)
     for img in tree.find_all('img'):
         src = img['src'].decode('utf-8').encode('Latin-1')
+        if src.lstrip('https://') == src:
+            continue
         print "Now downloading %s" % src
         img_parts = src.lstrip('https://').split('/')
         img_file = img_parts[-1]
         img_dir = os.path.join('img', os.path.join(*img_parts[:-1]))
+        print img_dir
         if not os.path.isdir(img_dir):
             os.makedirs(img_dir)
         try:
             img_path = os.path.join(img_dir, img_file)
-            img['src'] = img_path
             if not os.path.isfile(img_path):
-                urllib.urlretrieve(src, img_path)
+                r = requests.get(src)
+                if r.status_code == 200:
+                    with open(img_path, 'w') as f:
+                        f.write(r.content)
+                    img['src'] = img_path
         except:
             print "Error downloading %s" % src
     with open(file_out, 'w') as f:
